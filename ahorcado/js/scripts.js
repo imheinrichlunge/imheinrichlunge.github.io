20379e09e1e268eff3e7905f740033b9c2a7c2d8
Heinrich Lunge <ulomalopn63@gmail.com>
2022-09-27 15:04:21 -0500

    Rewritten and optimized code, missing, adding external fonts, drawing hangman, adding incorrect letters and virtual keyboard functionality

--- ahorcado/js/scripts.js
+++ ahorcado/js/scripts.js
@@ -1,3 +1,141 @@
+const words = ["sape"],
+    usedWords = [],
+    regex = /[A-zÑñ]/;
+
+const canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d"),
+    newGameButton = document.getElementById("new-game"),
+    input = document.getElementById("key-listener");
+
+let lineWidth = 25,
+    gap = 12,
+    randomNumber,
+    xAxis = [],
+    currentWord = "",
+    usedKeys = [],
+    usedKey = false,
+    hitCounter = 0,
+    errorCounter = 0;
+
+const getRandomNumber = () => {
+    let max = words.length - 1;
+    return Math.floor(Math.random() * (max - 0) + 0);
+};
+
+const createLine = (moveToX, moveToY, lineToX, lineToY) => {
+    ctx.beginPath();
+    ctx.moveTo(moveToX, moveToY);
+    ctx.lineTo(lineToX, lineToY);
+    ctx.closePath();
+    ctx.stroke();
+};
+
+const clearCanvas = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    createLine(69, 284, 213, 284);
+};
+
+const createKeyFields = () => {
+    let initialXAxis = (312 - (lineWidth * currentWord.length + gap * (currentWord.length - 1))) / 2;
+
+    for (let i = 0; i < currentWord.length; i++) {
+        xAxis.push(initialXAxis);
+        createLine(initialXAxis, 364, initialXAxis + lineWidth, 364);
+        initialXAxis = initialXAxis + lineWidth + gap;
+    }
+};
+
+const insertCorrectKeys = (position, key) => {
+    let keyWidth = ctx.measureText(key.toUpperCase()).width,
+        keyPosition = xAxis[position] + (lineWidth - keyWidth) / 2;
+    ctx.fillText(key.toUpperCase(), keyPosition, 354);
+};
+
+const drawHangman = () => {
+    const hangmanCoordinates = [];
+};
+
+const clearGame = () => {
+    usedKeys = [];
+    hitCounter = 0;
+    errorCounter = 0;
+    randomNumber = getRandomNumber();
+
+    if (words.length !== 0) {
+        currentWord = words[randomNumber];
+        usedWords.push(words[randomNumber]);
+        words.splice(randomNumber, 1);
+    } else {
+        words.push(...usedWords);
+        usedWords.splice(0, words.length);
+    }
+
+    input.value = "";
+    input.removeAttribute("disabled");
+    input.setAttribute("autofocus", "");
+    clearCanvas();
+};
+
+const hangman = (e) => {
+    let pressedKey = e.target.value.slice(-1).toLowerCase();
+
+    for (let i = 0; i < usedKeys.length; i++) {
+        if (usedKeys[i] === pressedKey) {
+            usedKey = true;
+        }
+    }
+    if (regex.test(pressedKey)) {
+        usedKeys.push(pressedKey);
+        switch (true) {
+            case usedKey:
+                usedKey = false;
+                break;
+            case currentWord.includes(pressedKey):
+                for (let i = 0; i < currentWord.length; i++) {
+                    if (currentWord[i] === pressedKey) {
+                        hitCounter++;
+                        insertCorrectKeys(i, pressedKey);
+                    }
+                }
+                if (hitCounter === currentWord.length) {
+                    input.setAttribute("disabled", "");
+                    alert("Ganó");
+                }
+                break;
+            default:
+                errorCounter++;
+                alert("Falló");
+                // paragraphs[1].innerText = `Errores: ${errorCounter} de 9`;
+                // input[1].value = input[1].value + pressedKey;
+                if (errorCounter === 9) {
+                    // paragraphs[3].innerText = `La palabra era ${currentWord}`;
+                    input.setAttribute("disabled", "");
+                    alert("Perdió");
+                }
+                break;
+        }
+    }
+};
+
+ctx.lineWidth = 4;
+ctx.lineCap = "round";
+ctx.strokeStyle = "#763C09";
+ctx.font = "28px serif";
+
+document.addEventListener("DOMContentLoaded", () => {
+    input.value = "";
+});
+newGameButton.addEventListener("click", () => {
+    clearGame();
+    createKeyFields();
+});
+document.addEventListener("click", () => {
+    input.focus();
+});
+
+input.addEventListener("input", hangman);
+
+/*
 // Dom Nodes
 const canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
@@ -26,6 +164,7 @@ const randomNumber = () => {
 
 const clearCanvas = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    createLine(69, 284, 213, 284);
 };
 
 const createLine = (moveToX, moveToY, lineToX, lineToY) => {
@@ -110,3 +249,4 @@ addEventListener("keydown", (e) => {
             break;
     }
 });
+*/
